bf8edcd900b9ac6d32c345e72cd5e215b09c67d2
JapoDeveloper <japo.developer@gmail.com>
2015-07-07 18:19:31 -0400

    He agregado el esqueleto del proyecto, y un archivo que contiene las herramientas que hasta el momento deben tener instaladas.

---
+++ DataCrediticiaProject/src/main/java/com/devcodespace/dcp/controllers/HistorialCrediticioController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.devcodespace.dcp.controllers;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author JapoDeveloper
+ */
+public class HistorialCrediticioController implements Serializable{
+
+    /**
+     * Creates a new instance of HistorialCrediticioController
+     */
+    public HistorialCrediticioController() {
+    }
+    
+}
